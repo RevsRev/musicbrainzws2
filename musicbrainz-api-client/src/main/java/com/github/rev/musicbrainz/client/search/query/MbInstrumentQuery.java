9fc29bb441541de6cdc46dbcd37a7b0a4f6be6cd
RevsRev <eddie.revell@yahoo.co.uk>
2025-01-25 15:06:35 +0000

    Added Query Fields for Instrument

--- musicbrainz-api-client/src/main/java/com/github/rev/musicbrainz/client/search/query/MbInstrumentQuery.java
+++ musicbrainz-api-client/src/main/java/com/github/rev/musicbrainz/client/search/query/MbInstrumentQuery.java
@@ -8,8 +8,58 @@ import java.util.Set;
  * MbQuery for Instruments.
  */
 public final class MbInstrumentQuery extends MbQuery<MbEntity.MbInstrument> {
+
+    /**
+     * (part of) any alias attached to the instrument (diacritics are ignored).
+     */
+    public static final String ALIAS = "alias";
+
+    /**
+     * (part of) the instrument's disambiguation comment.
+     */
+    public static final String COMMENT = "comment";
+
+    /**
+     * (part of) the description of the instrument (in English).
+     */
+    public static final String DESCRIPTION = "description";
+
+    /**
+     * The MBID of the instrument.
+     */
+    public static final String IID = "iid";
+
+    /**
+     * (part of) the instrument's name (diacritics are ignored).
+     */
+    public static final String INSTRUMENT = "instrument";
+
+    /**
+     * (part of) the instrument's name (with the specified diacritics).
+     */
+    public static final String INSTRUMENTACCENT = "instrumentaccent";
+
+    /**
+     * (part of) a tag attached to the instrument.
+     */
+    public static final String TAG = "tag";
+
+    /**
+     * The instrument's type.
+     */
+    public static final String TYPE = "type";
+
     @Override
     public Set<String> getQueryFields() {
-        return Set.of();
+        return Set.of(
+                ALIAS,
+                COMMENT,
+                DESCRIPTION,
+                IID,
+                INSTRUMENT,
+                INSTRUMENTACCENT,
+                TAG,
+                TYPE
+        );
     }
 }
