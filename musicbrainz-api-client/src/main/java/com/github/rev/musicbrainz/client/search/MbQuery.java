75d5f11821d998713a66e74fa04e8f84dae3d8de
RevsRev <eddie.revell@yahoo.co.uk>
2025-01-25 12:56:38 +0000

    Started building out MbQuery

--- musicbrainz-api-client/src/main/java/com/github/rev/musicbrainz/client/search/MbQuery.java
+++ musicbrainz-api-client/src/main/java/com/github/rev/musicbrainz/client/search/MbQuery.java
@@ -2,10 +2,65 @@ package com.github.rev.musicbrainz.client.search;
 
 import com.github.rev.musicbrainz.client.MbEntity;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
 /**
- * The query submitted to the MusicBrainz API as part of a search request.
+ * Base class for all queries submitted to the MusicBrainz API as part of a search request.
  *
  * @param <T> The entity type being browsed.
  */
-public class MbQuery<T extends MbEntity> {
+public abstract class MbQuery<T extends MbEntity> {
+
+    /**
+     * Map of Field to be queried to the value being queried.
+     */
+    private final HashMap<String, String> queryParams = new HashMap<>();
+    private final Set<String> queryFields = getQueryFields();
+
+    /**
+     * Instances should return a non-null map containing the permissible fields.
+     * @return Set of Field names that are permissible for this query.
+     */
+    protected abstract Set<String> getQueryFields();
+
+    /**
+     * Add a Field and Value to the query.
+     * @param field The Field name.
+     * @param value The Value.
+     * @throws InvalidQueryFieldException If the Field name is not permissible for this query.
+     */
+    public final void add(final String field, final String value) throws InvalidQueryFieldException {
+        if (!queryFields.contains(field)) {
+            throw new InvalidQueryFieldException(field);
+        }
+        queryParams.put(field, value);
+    }
+
+    /**
+     * @return The query String to be submitted to the API.
+     */
+    public final String asQueryString() {
+        StringBuilder sb = new StringBuilder();
+        for (Map.Entry<String, String> keyValue : queryParams.entrySet()) {
+            sb.append(keyValue.getKey());
+            sb.append(':');
+            sb.append(keyValue.getValue());
+        }
+        return sb.toString();
+    }
+
+    /**
+     * Checked exception to encapsulate errors in constructing a query.
+     */
+    public static final class InvalidQueryFieldException extends Exception {
+
+        /**
+         * @param fieldName The field that was invalid for this query.
+         */
+        public InvalidQueryFieldException(final String fieldName) {
+            super(String.format("Field '%s' is not valid for this query.", fieldName));
+        }
+    }
 }
