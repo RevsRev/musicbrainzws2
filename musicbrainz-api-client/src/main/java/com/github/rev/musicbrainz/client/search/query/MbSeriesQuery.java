aa98d2257a3f4b1d46bcca984462b14920d8c4ae
RevsRev <eddie.revell@yahoo.co.uk>
2025-01-25 16:03:06 +0000

    Added Fields for Series Query

--- musicbrainz-api-client/src/main/java/com/github/rev/musicbrainz/client/search/query/MbSeriesQuery.java
+++ musicbrainz-api-client/src/main/java/com/github/rev/musicbrainz/client/search/query/MbSeriesQuery.java
@@ -8,8 +8,52 @@ import java.util.Set;
  * MbQuery for Series.
  */
 public final class MbSeriesQuery extends MbQuery<MbEntity.MbSeries> {
+
+    /**
+     * (part of) any alias attached to the series (diacritics are ignored).
+     */
+    public static final String ALIAS = "alias";
+
+    /**
+     * (part of) the series' disambiguation comment.
+     */
+    public static final String COMMENT = "comment";
+
+    /**
+     * (part of) the series' name (diacritics are ignored).
+     */
+    public static final String SERIES = "series";
+
+    /**
+     * (part of) the series' name (with the specified diacritics).
+     */
+    public static final String SERIES_ACCENT = "seriesaccent";
+
+    /**
+     * The series' MBID.
+     */
+    public static final String SID = "sid";
+
+    /**
+     * (part of) a tag attached to the series.
+     */
+    public static final String TAG = "tag";
+
+    /**
+     * The series' type.
+     */
+    public static final String TYPE = "type";
+
     @Override
     public Set<String> getQueryFields() {
-        return Set.of();
+        return Set.of(
+                ALIAS,
+                COMMENT,
+                SERIES,
+                SERIES_ACCENT,
+                SID,
+                TAG,
+                TYPE
+        );
     }
 }
