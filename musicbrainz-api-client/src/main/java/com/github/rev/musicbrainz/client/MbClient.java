3b42323208a50d5605b2247eaca3ea65bb3e70a0
RevsRev <eddie.revell@yahoo.co.uk>
2025-01-26 23:11:06 +0000

    Started fleshing out the MbClient

--- musicbrainz-api-client/src/main/java/com/github/rev/musicbrainz/client/MbClient.java
+++ musicbrainz-api-client/src/main/java/com/github/rev/musicbrainz/client/MbClient.java
@@ -5,4 +5,31 @@ package com.github.rev.musicbrainz.client;
  * http responses into MbResponses.
  */
 public final class MbClient {
+
+    private static final String DEFAULT_PROTOCOL = "http";
+    private static final String DEFAULT_HOST = "musicbrainz.org";
+    private static final String DEFAULT_PATH_PREFIX = "ws";
+    private static final String DEFAULT_VERSION = "2";
+    private static final String DEFAULT_ENDPOINT = DEFAULT_PROTOCOL + "/"
+            + DEFAULT_HOST + "/"
+            + DEFAULT_PATH_PREFIX + "/"
+            + DEFAULT_VERSION;
+
+    private final String endpoint;
+
+    /**
+     * Construct a client for the default MusicBrainz endpoint.
+     */
+    public MbClient() {
+        this(DEFAULT_ENDPOINT);
+    }
+
+    /**
+     * Construct a client for a specific endpoint hosting the MusicBrainz API.
+     * @param endpoint The endpoint hosting the MusicBrainz API.
+     */
+    public MbClient(final String endpoint) {
+        this.endpoint = endpoint;
+    }
+
 }
